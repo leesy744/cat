65be0b2fc241b831457c43ddfbe10641ad0521eb
leesy744 <78004293+leesy744@users.noreply.github.com>
2022-08-17 12:25:31 +0900

    dataquestion

---
+++ src/assets/data/questiondata.js
@@ -0,0 +1,52 @@
+//data 객체 배열로 생성
+
+export const QuestionData = [
+    {
+        id: 1,
+        title: "문제1",
+        answera:"답변1",
+        answerb:"답변2",
+        type: "EI",
+    },
+    {
+        id: 2,
+        title: "선호하는 여행유형은 무엇인가요?",
+        answera:"도시형",
+        answerb:"휴양지형",
+        answerc:"상관없음",
+        type: "EI",
+    },
+    {
+        id: 3,
+        title: "여행 도착지에서 났으면 하는 향기는 무엇인가요?",
+        answera:"꽃",
+        answerb:"과일, 달달한 향",
+        answerc:"물내음, 비누",
+        answerd:"풀",
+        type: "EI",
+    },
+    {
+        id: 4,
+        title: "여행지에서 사용할 장소는 어디인가요?",
+        answera:"거실",
+        answerb:"침실",
+        answerc:"실외",
+        type: "EI",
+    },
+    {
+        id: 5,
+        title: "여행지에서 사용하고 싶은 시간은 언제인가요?",
+        answera:"낮",
+        answerb:"밤",
+        type: "EI",
+    },
+    {
+        id: 6,
+        title: "여행은 어느 계절에 떠나실 생각이신가요?",
+        answera:"봄",
+        answerb:"여름",
+        answerc:"가을",
+        answerd:"겨울",
+        type: "EI",
+    }
+]

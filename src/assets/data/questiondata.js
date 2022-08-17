1f0f6ce7577dd9242bcf0dcb13b8520b3c73e313
leesy744 <78004293+leesy744@users.noreply.github.com>
2022-08-17 16:52:43 +0900

    Add files via upload

--- src/assets/data/questiondata.js
+++ src/assets/data/questiondata.js
@@ -1,23 +1,17 @@
 //data 객체 배열로 생성
 
 export const QuestionData = [
+   
     {
         id: 1,
-        title: "문제1",
-        answera:"답변1",
-        answerb:"답변2",
-        type: "EI",
-    },
-    {
-        id: 2,
-        title: "선호하는 여행유형은 무엇인가요?",
+        title: "선호하는 여행 유형은 무엇인가요?",
         answera:"도시형",
         answerb:"휴양지형",
         answerc:"상관없음",
-        type: "EI",
+        type: "1211",
     },
     {
-        id: 3,
+        id: 2,
         title: "여행 도착지에서 났으면 하는 향기는 무엇인가요?",
         answera:"꽃",
         answerb:"과일, 달달한 향",
@@ -26,7 +20,7 @@ export const QuestionData = [
         type: "EI",
     },
     {
-        id: 4,
+        id: 3,
         title: "여행지에서 사용할 장소는 어디인가요?",
         answera:"거실",
         answerb:"침실",
@@ -34,14 +28,14 @@ export const QuestionData = [
         type: "EI",
     },
     {
-        id: 5,
+        id: 4,
         title: "여행지에서 사용하고 싶은 시간은 언제인가요?",
         answera:"낮",
         answerb:"밤",
         type: "EI",
     },
     {
-        id: 6,
+        id: 5,
         title: "여행은 어느 계절에 떠나실 생각이신가요?",
         answera:"봄",
         answerb:"여름",

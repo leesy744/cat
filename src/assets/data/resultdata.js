8b843a7dbf6738915390bc173c57324211b70c32
leesy744 <78004293+leesy744@users.noreply.github.com>
2022-08-18 10:55:43 +0900

    up

---
+++ src/assets/data/resultdata.js
@@ -0,0 +1,12 @@
+import candle from '../candle.jpg';
+
+export const ResultData = [
+    {
+        id : 1,
+        name: 'ㅇㅇ캔들',
+        best : 'ㅇㅇ캔들',
+        desc : 'dkfajdksfjskjfsjfiwjfskdjfalkdjfaksdjflsjfskdfjdkjfsdkjfkdfjdkfjd',
+        image : `${candle}`,
+    },
+    
+]
